2e4504f587cf33039211d8b7095ad39a3bc72095
handan14 <handan14@student.westerdals.no>
2015-03-12 14:11:05 +0100

    Du kan nå ikke reservere rom med tomme felt

--- Kode/php/Layout.php
+++ Kode/php/Layout.php
@@ -6,11 +6,11 @@ require 'config.php';
 			$dato = $tid = "";
 			$ant = $rom = 0;
 			$proj = false;
-			$dato = $_POST["bday"];
-			$tid1 = $_POST["from_time"];
-			$tid2 = $_POST["to_time"];
-			$ant = $_POST["quantity"];
-			$rom = $_POST["rom"];
+			$dato = test_input($_POST["bday"]);
+			$tid1 = test_input($_POST["from_time"]);
+			$tid2 = test_input($_POST["to_time"]);
+			$ant = test_input($_POST["quantity"]);
+			$rom = test_input($_POST["rom"]);
 			echo $bruker . "<br>" . $dato . "<br>" . $tid1 . "<br>" . $tid2 . "<br>" . $ant . "<br>" . $rom . "<br>";
 			if ($_POST["Prosjektor"] == "Ja") {
 				echo "Prosjektor ja<br>";
@@ -19,15 +19,19 @@ require 'config.php';
 				echo "Prosjektor nei<br>";
 				$prosjektor = false;
 			}
-			if ($tid1 < $tid2) {
-				if ($dato > date("Y-m-d") || $dato == date("Y-m-d") && strtotime($tid1) > time()) {
-					$sql = $database->prepare("INSERT INTO reservasjon (romnummer, dato, fratid, tiltid, student, prosjektor) VALUES ('$rom', '$dato', '$tid1', '$tid2', '$bruker', $prosjektor);");
-					$sql->execute();
-				} else {
-					echo "DATOEN ER FEIL";
-				}
+			if ($dato == "" || $tid1 == "" || $tid2 == "" || $rom == 0 || $ant == 0 || $bruker == "") {
+				echo "Noe gikk galt!";
 			} else {
-				echo "TIDEN ER FEIL!";
+				if ($tid1 < $tid2) {
+					if ($dato > date("Y-m-d") || $dato == date("Y-m-d") && strtotime($tid1) > time()) {
+						$sql = $database->prepare("INSERT INTO reservasjon (romnummer, dato, fratid, tiltid, student, prosjektor) VALUES ('$rom', '$dato', '$tid1', '$tid2', '$bruker', $prosjektor);");
+						$sql->execute();
+					} else {
+						echo "DATOEN ER FEIL";
+					}
+				} else {
+					echo "TIDEN ER FEIL!";
+				}
 			}
 		} else {
 			header("Location: login.php");
