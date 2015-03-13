afce8a4ec2cf97b3d46ec91784a9c574da6d7994
handan14 <handan14@student.westerdals.no>
2015-03-13 13:44:56 +0100

    legger til php- og css-kode som mystisk forsvant sist gang jeg pulla

---
+++ Kode/php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_host = "127.0.0.1";
+$db_name = "rom";
+$db_user = "root";
+$db_pass = "";
+
+$database = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlentities($data);
+	return $data;
+}
+?>
---
+++ Kode/php/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../css/Login.css">
+<?php
+	require 'config.php';
+	$studentnummer = $passord = $bruker = "";
+	$studFeil = $passFeil = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["studentnummer"])) {
+			$studFeil = "Obligatorisk!";
+		} else {
+			$studentnummer = test_input($_POST["studentnummer"]);
+		}
+		if (empty($_POST["passord"])) {
+			$passFeil = "Obligatorisk!";
+		} else {
+			$passord = test_input($_POST["passord"]);
+		}
+		if ($studFeil == "" && $passFeil == "") {
+			$sql = $database->prepare("SELECT * FROM bruker WHERE studentnummer='$studentnummer' AND passord='$passord';");
+			$sql->setFetchMode(PDO::FETCH_OBJ);
+			$sql->execute();
+			while ($testvar = $sql->fetch()) {
+				$bruker = $testvar->studentnummer;
+			}
+			if ($bruker == "") {
+				echo "Ingen bruker";
+			} else {
+				setcookie("user", $bruker, time() + 3600, "/");
+				header("Location: ../html/Layout.html");
+			}
+		}
+	}
+?>
+</head>
+<body>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" accept-charset="utf-8">
+	Studentnummer: <input type="text" name="studentnummer"><span class="error">* <?php echo $studFeil; ?></span><br><br>
+	Passord: <input type="password" name="passord"><span class="error">* <?php echo $passFeil; ?></span><br><br>
+	<input type="submit">
+</form>
+<br>
+<form action="registrer.php"><input value="Registrer deg" type="submit"></form>
+
+</body>
+</html>
