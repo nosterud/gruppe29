35446e174499f6145c8d2e03301c1b0db7f21682
handan14 <handan14@student.westerdals.no>
2015-03-17 13:30:34 +0100

    La til php-kode til kartet og det nye designet på layout siden

---
+++ Kode/php/rom.php
@@ -0,0 +1,25 @@
+<?php
+require 'config.php';
+$nummer = test_input($_GET["nummer"]);
+$sql = $database->prepare("SELECT romnummer, dato, fratid, tiltid, student, prosjektor, max(antall) as num FROM reservasjon WHERE romnummer = $nummer GROUP BY student, romnummer, dato, fratid, tiltid ORDER BY dato, fratid ASC;");
+$sql->setFetchMode(PDO::FETCH_OBJ);
+$sql->execute();
+$count = 0;
+while ($item = $sql->fetch()) {
+	if ($item->dato == date("Y-m-d")) {
+		if (strtotime($item->fratid) < time() && time() < strtotime($item->tiltid)) {
+			$count += $item->num;
+			if ($item->prosjektor == 1) {
+				echo "<p>Prosjektoren er opptatt! <br /></p>";
+			}
+		}
+	}
+}
+echo "<p>$count / 4</p>";
+if ($count == 4) {
+	echo "<style>body{background-color:red;}</style>";
+} else if($count > 0) {
+	echo "<style>body{background-color:yellow;}</style>";
+}
+?>
+
